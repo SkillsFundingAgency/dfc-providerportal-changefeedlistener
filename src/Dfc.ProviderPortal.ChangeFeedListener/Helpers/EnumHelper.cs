1a696780fee66e90f263773f232b084180f12ecf
Ian Rose <ian.findacourse@gmail.com>
2019-09-16 15:52:23 +0100

    Added EnumHelper

---
+++ src/Dfc.ProviderPortal.ChangeFeedListener/Helpers/EnumHelper.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+
+namespace Dfc.ProviderPortal.ChangeFeedListener.Helpers
+{
+    public static class EnumHelper
+    {
+        /// <summary>
+        /// Gets the Description attribute on an enum value
+        /// </summary>
+        public static string Description(this Enum value)
+        {
+            object attribute = value.GetType()
+                                   ?.GetMember(value.ToString())
+                                   ?.FirstOrDefault()
+                                   ?.GetCustomAttributes(typeof(DescriptionAttribute), false)
+                                   ?.FirstOrDefault();
+            return ((DescriptionAttribute)attribute)?.Description;
+        }
+    }
+}
