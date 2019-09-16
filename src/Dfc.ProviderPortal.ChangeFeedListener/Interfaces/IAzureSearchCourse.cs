bf5222a00d41f563e50dc8214b4492d2fc47c813
Ian Rose <ian.findacourse@gmail.com>
2019-09-16 15:52:38 +0100

    wip

--- src/Dfc.ProviderPortal.ChangeFeedListener/Interfaces/IAzureSearchCourse.cs
+++ src/Dfc.ProviderPortal.ChangeFeedListener/Interfaces/IAzureSearchCourse.cs
@@ -15,8 +15,18 @@ namespace Dfc.ProviderPortal.ChangeFeedListener.Interfaces
         string VenueAddress { get; set; }
         GeographyPoint VenueLocation { get; set; }
         string VenueAttendancePattern { get; set; }
+        //string VenueAttendancePatternDescription { get; set; }
         string ProviderName { get; set; }
         string Region { get; set; }
         int? Status { get; set; }
+        string VenueStudyMode { get; set; }
+        //string VenueStudyModeDescription { get; set; }
+        string DeliveryMode { get; set; }
+        //string DeliveryModeDescription { get; set; }
+        DateTime? StartDate { get; set; }
+        string VenueTown { get; set; }
+        int? Cost { get; set; }
+        string CostDescription { get; set; }
+        string CourseText { get; set; }
     }
 }
