87c7230968d7fa1677c2e2a23ac2bc049029e32f
Stephen Yates <SYatesSoftware@gmail.com>
2019-11-13 11:25:58 +0000

    Report changes

---
+++ src/Dfc.ProviderPortal.ChangeFeedListener/Functions/UpdateCourseMigrationReports.cs
@@ -0,0 +1,39 @@
+using Dfc.ProviderPortal.ChangeFeedListener.Helpers;
+using Dfc.ProviderPortal.ChangeFeedListener.Interfaces;
+using Dfc.ProviderPortal.Packages.AzureFunctions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using Dfc.ProviderPortal.ChangeFeedListener.Models;
+using Dfc.ProviderPortal.ChangeFeedListener.Services;
+
+namespace Dfc.ProviderPortal.ChangeFeedListener.Functions
+{
+    public static class UpdateCourseMigrationReports
+    {
+        [FunctionName("UpdateCourseMigrationReports")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log,
+            [Inject] ReportGenerationServiceResolver reportGenerationServiceResolver)
+        {
+            var reportGenerationService = reportGenerationServiceResolver(ProcessType.Course);
+
+            try
+            {
+                await reportGenerationService.GenerateAllReports(log);
+
+                return new OkResult();
+
+            }
+            catch (Exception e)
+            {
+                return new InternalServerErrorObjectResult(new Exception($"Failed to update all reports", e));
+            }
+        }
+    }
+}
