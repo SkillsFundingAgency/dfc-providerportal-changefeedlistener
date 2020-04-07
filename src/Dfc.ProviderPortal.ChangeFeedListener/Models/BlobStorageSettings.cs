e110cd5d4c5da7539efe544156da5dcd59faf7aa
MrKevJoy <60096576+MrKevJoy@users.noreply.github.com>
2020-04-07 10:06:44 +0100

    Ptcd 177 fa (#75)
    * initial commit of fa.
    
    * change.
    
    * removed disabled flags
    
    * removed unwanted usings.
    
    * changes
    
    * changes to remove old data in index.
    
    * recommeded changes.
    
    Co-authored-by: MrKevJoy <MrKevinJoy@outlook.com>

---
+++ src/Dfc.ProviderPortal.ChangeFeedListener/Models/BlobStorageSettings.cs
@@ -0,0 +1,13 @@
+﻿using Dfc.ProviderPortal.ChangeFeedListener.Helpers;
+using Dfc.ProviderPortal.ChangeFeedListener.Interfaces;
+
+namespace Dfc.ProviderPortal.ChangeFeedListener.Models
+{
+    public class BlobStorageSettings : IBlobStorageSettings
+    {
+        public string AccountName { get; set; }
+        public string AccountKey { get; set; }
+        public string Container { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
